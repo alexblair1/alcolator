18048f4ce349ab5abb46caf868dc47881a42bc47
FirstName LastName <alexblair190@gmail.com>
2015-05-19 11:08:29 -0600

    checkpoint-18-new

---
+++ Alcolator-project/Alcolator/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Alcolator
+//
+//  Created by Stephen Blair on 5/18/15.
+//  Copyright (c) 2015 blairgraphix. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Alcolator-project/Alcolator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Alcolator
+//
+//  Created by Stephen Blair on 5/18/15.
+//  Copyright (c) 2015 blairgraphix. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
