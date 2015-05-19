095e9a1c7964fb03a2bd1ec71e04e3f2ac986b10
FirstName LastName <alexblair190@gmail.com>
2015-05-19 13:36:16 -0600

    checkpoint 19 complete

---
+++ Alcolator-project/Alcolator/WhiskeyViewController.h
@@ -0,0 +1,13 @@
+//
+//  WhiskeyViewController.h
+//  Alcolator
+//
+//  Created by Stephen Blair on 5/19/15.
+//  Copyright (c) 2015 blairgraphix. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface WhiskeyViewController : ViewController
+
+@end
